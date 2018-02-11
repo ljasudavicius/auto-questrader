2e937ede76b8d4abe1065ab3188849fff6a634e7
Luke <ljasudavicius@konradgroup.com>
2018-02-10 18:20:04 -0600

    testing

--- AutoQuestraderWeb/AutoQuestraderWeb/Hubs/ChatHub.cs
+++ AutoQuestraderWeb/AutoQuestraderWeb/Hubs/ChatHub.cs
@@ -8,6 +8,13 @@ namespace AutoQuestraderWeb.Hubs
 {
     public class ChatHub : Hub
     {
+        public override async Task OnConnectedAsync()
+        {
+            var email = Context.Connection.GetHttpContext().Request.Query["email"];
+
+            await Clients.All.InvokeAsync("broadcastMessage", $"{Context.ConnectionId} joined");
+        }
+
         public void Send(string name, string message)
         {
             // Call the broadcastMessage method to update clients.
